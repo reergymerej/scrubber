e8e65a020839588bfcd3ff4abcc3bcf8a46a28a8
Jeremy Greer <jex.grizzle@gmail.com>
2020-07-23 07:56:01 -0700

    Change content of file during move

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ Redact/replace content in text files.
 * upload to S3
 * lambda scrubs content
 * output to S3, original trashed
+
+
+```
+aws s3 cp file.txt s3://jex-scrubber/in/file.txt
+```
+
+## Next
+
+Remove blacklisted patterns
--- src/lambda_function.py
+++ src/lambda_function.py
@@ -4,29 +4,36 @@ import boto3
 
 s3 = boto3.resource('s3')
 
+def get_new_content(string):
+    return "This is new.\n\n" + string
 
 def lambda_handler(event, context):
     # print("Received event: " + json.dumps(event, indent=2))
 
-    # Get the object from the event and show its content type
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(
         event['Records'][0]['s3']['object']['key'],
         encoding='utf-8'
     )
+    destintation_key = key.replace("in/", "out/")
 
     try:
-        copy_source = bucket + "/" + key
-        print("source:", copy_source)
-        destintation_key = key.replace("in/", "out/")
-        s3.Object(bucket, destintation_key).copy_from(
-            CopySource = copy_source
+        body = s3.Object(bucket, key).get()['Body'].read().decode()
+        # print("body", body)
+
+        new_body = get_new_content(body)
+        print("new body", new_body)
+
+        s3.Object(bucket, destintation_key).put(
+            Body=new_body
         )
+
+        # remove original
         s3.Object(bucket, key).delete()
         print("moved")
         return 0
+
     except Exception as e:
         print(e)
-        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+        print('Error getting object {} from bucket {}.'.format(key, bucket))
         raise e
-
